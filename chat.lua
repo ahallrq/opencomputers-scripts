b2cba1001b82632e0af5f719eb7383932e1d9129
Andrew Hall <andrew.hall649@gmail.com>
2018-09-29 10:06:10 +0000

    Fix a bug with dodgy typing. A printing issue still exists when the user types a big line

--- chat.lua
+++ chat.lua
@@ -5,16 +5,39 @@ local io = require("io")
 local term = require("term")
 local text = require("text")
 
-
 local chat_port = 9999
 local chat_name = "YOUR NAME HERE"
 
 local m = component.modem
 m.open(9999)
 
+-- Simple lock
+
+SimpleLock = { isLocked = 0,
+  get = function (self)
+          if self.isLocked == 0 then
+            self.isLocked = 1
+          else
+            repeat
+              os.sleep(0.01)
+            until self.isLocked == 0
+          end
+        end,
+  release = function (self)
+              if self.isLocked == 0 then
+                error("Lock has already been released!")
+              else
+                self.isLocked = 1
+              end
+            end
+}
+
+--
+
 -- Print function comes from here:
 -- https://github.com/OpenPrograms/Kenny-Programs/blob/master/OpenComputers/irc.lua
 function print(message, overwrite)
+  SimpleLock:get()
   local w, h = component.gpu.getResolution()
   local line
   repeat
@@ -26,21 +49,25 @@ function print(message, overwrite)
     component.gpu.fill(1, h - 1, w, 1, " ")
     component.gpu.set(1, h - 1, line)
   until not message or message == ""
+  SimpleLock:release()
 end
 
-timer = nil
+t = nil
 
 res, err = pcall(function()
   term.clear()
   print("Chat v1")
 
-  timer = event.timer(0.25, function()
-    local res, _, from, port, _, user, msg = event.pull(0.05, "modem_message")
-    if res ~= nil then
-        s = "<"..user.."> "..msg
-        print(s, true)
+  t = thread.create(function()
+    while true do
+        local res, _, from, port, _, user, msg = event.pull("modem_message")
+        if res ~= nil then
+            s = "<"..user.."> "..msg
+            print(s, true)
+        end
+        os.sleep(0.05)
     end
-  end, math.huge)
+  end)
 
   while true do
     local w, h = component.gpu.getResolution()
@@ -61,7 +88,7 @@ res, err = pcall(function()
 end)
 
 if timer then
-  event.cancel(timer)
+  t:kill()
 end
 
 if not res then
